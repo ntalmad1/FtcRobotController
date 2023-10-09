06926720e1f7115d073e65c67d91390385636e1b
Noah Talmadge <ntalmad1@gmail.com>
2023-10-09 16:08:37 -0700

    wip

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/component/event/gp2_left_stick_y/Gp2_LeftStickYHandler.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.library.component.event.gp2_left_stick_y;
+
+import org.firstinspires.ftc.teamcode.library.component.event.EventHandler;
+
+/**
+ *
+ */
+public interface Gp2_LeftStickYHandler extends EventHandler
+{
+    /**
+     *
+     * @param event
+     */
+    void onGp2_LeftStickX (Gp2_LeftStickYEvent event);
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/component/event/gp2_right_stick_y/bar.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.library.component.event.gp2_right_stick_y;
+
+public class bar {
+}
