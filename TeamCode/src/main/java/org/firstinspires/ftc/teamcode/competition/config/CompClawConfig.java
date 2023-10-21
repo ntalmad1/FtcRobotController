0b3de52557fefd320b8f98846dccd8f3816e4be6
Noah Talmadge <ntalmad1@gmail.com>
2023-10-20 19:18:54 -0700

    comp packages

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/config/CompClawConfig.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.competition.config;
+
+import org.firstinspires.ftc.teamcode.library.claw.ClawConfig;
+
+/**
+ *
+ */
+public class CompClawConfig extends ClawConfig {
+
+    public CompClawConfig () {
+
+    }
+
+}
