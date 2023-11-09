96406b76506eee9fb66d87a0cd711eaae136f15f
Noah Talmadge <ntalmad1@uoregon.edu>
2023-11-09 10:45:44 -0800

    Pixel Catcher

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/library/pixelcatcher/PixelCatcherConfig.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.library.pixelcatcher;
+
+import org.firstinspires.ftc.library.Control;
+
+/**
+ *
+ */
+public class PixelCatcherConfig {
+
+    /**
+     *
+     */
+    public double leftArmOpenedPos;
+
+    /**
+     *
+     */
+    public double leftArmClosedPos;
+
+    /**
+     *
+     */
+    public double leftArmInitPos;
+
+    /**
+     */
+    public Control leftArmToggle = Control.Gp1_LeftTrigger_Down;
+
+    /**
+     *
+     */
+    public double rightArmOpenedPos;
+
+    /**
+     *
+     */
+    public double rightArmClosedPos;
+
+    /**
+     *
+     */
+    public double rightArmInitPos;
+
+
+    /**
+     */
+    public Control rightArmToggle = Control.Gp1_RightTrigger_Down;
+}
