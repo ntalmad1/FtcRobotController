6d234e6a5bd2235b36c4802fd46a705b001f6111
IsaacT <isaactalmadge@gmail.com>
2024-02-16 21:51:56 -0800

    motortest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/MotorTest.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.metalheads;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.library.IsaacBot;
+
+@TeleOp(name="MotorTest", group="Linear OpMode")
+//@Disabled
+public class MotorTest extends IsaacBot {
+
+    //537.7 tick per revolution
+
+    private DcMotor motor = null;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        motor = hardwareMap.get(DcMotor.class, "actuatorMotor");
+        motor.setDirection(DcMotor.Direction.FORWARD);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        waitForStart();
+
+        while (this.opModeIsActive()) {
+
+            //soft
+
+            if (gamepad1.right_stick_x > 0) {
+
+                motor.setTargetPosition(this.motor.getCurrentPosition() + 80);
+
+                motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                motor.setPower(1*gamepad1.right_stick_x);
+            }
+
+            if (gamepad1.right_stick_x < 0) {
+
+                motor.setTargetPosition(this.motor.getCurrentPosition() - 80);
+                motor.setDirection(DcMotor.Direction.REVERSE);
+                motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+                motor.setPower(Math.abs(1*gamepad1.right_stick_x));
+            }
+
+
+            telemetry.addData("Current Position",  "%7d", motor.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
