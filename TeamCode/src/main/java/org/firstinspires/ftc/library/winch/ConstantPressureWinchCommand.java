22a7c3803584180a4bf1a892c0b8de489fe38340
Noah Talmadge <ntalmad1@gmail.com>
2024-02-29 23:13:41 -0800

    constant winch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/library/winch/ConstantPressureWinchCommand.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.library.winch;
+
+import org.firstinspires.ftc.library.component.command.Command;
+
+/**
+ *
+ */
+public class ConstantPressureWinchCommad extends Command {
+}
