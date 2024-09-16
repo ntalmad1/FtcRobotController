d9f0072a5b91c9fc6922cceb0588be5c1c68854d
IsaacT <isaactalmadge@gmail.com>
2024-09-15 21:31:13 -0700

    Intake Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/ArmTest.java
@@ -1,5 +1,24 @@
 package org.firstinspires.ftc.teamcode.metalheads;
 
-public class ArmTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 
+@TeleOp(name="ArmTest", group="Tests")
+// @Disabled
+public class ArmTest extends LinearOpMode {
+
+
+
+    public void runOpMode() {
+
+        CRServo intake = hardwareMap.get(CRServo.class, "intakeMain");
+
+        waitForStart();
+        while (this.opModeIsActive()) {
+
+            intake.setPower(gamepad1.left_stick_y);
+
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/ContinuousServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/ContinuousServoTest.java
@@ -1,16 +1,24 @@
 package org.firstinspires.ftc.teamcode.metalheads;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
 
-@TeleOp(name="OmniPodTest", group="Tests")
+@TeleOp(name="ContinuousServoTest", group="Tests")
 // @Disabled
 public class ContinuousServoTest extends LinearOpMode {
 
+
+
     public void runOpMode() {
-        private 
+
+        CRServo intake = hardwareMap.get(CRServo.class, "intakeMain");
+
+        waitForStart();
+        while (this.opModeIsActive()) {
+
+            intake.setPower(gamepad1.left_stick_y);
+
+        }
     }
 }
