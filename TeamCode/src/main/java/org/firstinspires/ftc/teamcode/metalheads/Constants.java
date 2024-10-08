fc0c1ba04f184f9668b1ed25ad3a94b2584d6171
Noah Talmadge <ntalmad1@uoregon.edu>
2024-10-08 16:48:25 -0700

    constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/Constants.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.metalheads;
+
+/**
+ *
+ */
+public class Constants {
+
+    /**
+     *
+     */
+    public static final double INTAKE_VSERVO_INIT_POS = 0.364;
+    
+    /**
+     * Hidden constructor - make class essentially static
+     */
+    private Constants() {
+    }
+}
